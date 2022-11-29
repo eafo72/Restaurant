901205e85ec647bf2b165ba05f6475e728e4a2ce
alex <eafo72@gmail.com>
2022-11-28 19:00:00 -0600

    cambios 28112022

---
+++ src/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.css";
+import { Container, Col, Row } from "react-bootstrap";
+import "./css/Social_icons.css";
+import "./css/Footer_dark.css";
+import "./fonts/ionicons.min.css";
+import logo_footer from "./images/logo_restaurant.png";
+
+
+export const Footer = () => {
+
+    const link_facebook = 'https://www.facebook.com';
+    const link_youtube = 'https://www.youtube.com';
+   
+
+
+
+  return (
+    <footer className="footer-dark">
+      <Container>
+        <Row>
+          <Col xs={12} lg={1} style={{ padding: 0 }} className="d-flex align-items-center justify-content-center">
+            <p id="logo_footer">
+              <img src={logo_footer} alt="" />
+            </p>
+          </Col>
+          <Col xs={12} lg={5} style={{ padding: 0 }} className="d-flex align-items-center justify-content-center">
+            <p id="marca_footer">
+              {" "}
+              Restaurant ® 2022 Todos los derechos reservados.
+            </p>
+          </Col>
+          <Col xs={12} lg={3} style={{ padding: 0 }} className="d-flex align-items-center justify-content-center">
+            <p
+              style={{
+                textAlign: "center",
+                fontSize: ".75rem",
+                paddingTop: "8px",
+              }}
+            >
+              Proyecto 4
+            </p>
+          </Col>
+          <Col xs={12} lg={3} style={{ padding: 0 }} className="d-flex align-items-center justify-content-center">
+            <div className="item social">
+              <a href={link_facebook} target="_blank" rel="noopener noreferrer">
+                <i className="icon ion-social-facebook"></i>
+              </a>
+              
+              <a href={link_youtube} target="_blank" rel="noopener noreferrer">
+                <i className="icon ion-social-youtube-outline"></i>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+import logo from "./images/logo_restaurant.png";
+
+export function MyNavbar() {
+  return (
+    <Navbar expand="md">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="" style={{width: "60px", height: "60px"}} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Contacto">Contacto</Nav.Link>
+            <Nav.Link href="/Reservar">Reservar</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
